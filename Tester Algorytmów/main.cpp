1380a13886fe58537d99b1ab29c9b85c2c98a77d
Tomasz Kremiec <tkremiec@gmail.com>
2022-03-12 13:51:29 +0100

    Added new algorithm test app

---
+++ Tester Algorytmów/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+int main()
+{
+    int n, i, szukana, *tab;
+    tab = new int[n];
+    bool znaleziono;
+
+    cout << "Jaki rozmiar tablicy ?: ";
+    cin >> n;
+
+    srand(time(NULL));
+    for (i = 0; i < n; i++)
+    {
+        cout << "Podaj element o indeksie " << i << ": ";
+        cin >> tab[i];
+    }
+
+    cout << "Nacisnij dowolny klawisz zeby rozpoczac" << endl;
+    system("pause");
+
+    // ponizej badany algorytm
+    znaleziono = 0;
+    cout << "Czego szukasz?: ";
+    cin >> szukana;
+
+    for (i = 0; i < n; i++)
+    {
+        if (tab[i] == szukana)
+        {
+            cout << "Znaleziono pod indeksem " << i << endl;
+            znaleziono = 1;
+        }
+    }
+
+    if (!znaleziono)
+    {
+        cout << "Niczego nie znaleziono";
+    }
+    else
+    {
+        cout << "Znaleziono";
+    }
+    system("Pause");
+}
